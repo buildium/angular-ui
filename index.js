6b8b39a4ef79293c49cb794523b4a1be1e36904c
Jesse Weisbeck <jesse.weisbeck@buildium.com>
2017-04-04 09:49:45 -0400

    Adds event attribute directive to package. Requires jquery

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 let moduleName = 'buildium.angular-ui';
 
 angular.module(moduleName, [
-    require('./compile-dynamic-html')
+    require('./compile-dynamic-html'),
+    require('./event')
 ]);
 
 module.exports = moduleName;
