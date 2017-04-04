8b19fc808422120c7c8dd92ac3a52e6d42134a1e
Edwin <echow6@gmail.com>
2017-04-04 11:24:43 -0400

    Loading src (#5)
    * done
    
    * update vesrion
    
    * version
    
    * version
    
    * version1
    
    * version2
    
    * add jquery
    
    * revert gitignore
    
    * no new line
    
    * newline
    
    * let
    
    * version

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let moduleName = 'buildium.angular-ui';
 
 angular.module(moduleName, [
     require('./compile-dynamic-html'),
-    require('./copy-to-clipboard')
+    require('./copy-to-clipboard'),
+    require('./loading-src')
 ]);
 
 module.exports = moduleName;
