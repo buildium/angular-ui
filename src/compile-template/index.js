67c80fb42d6410790e6edf7fea9ca2dd0b80b7a3
Ryan Pierce <ryan.pierce@buildium.com>
2017-04-04 14:52:14 -0400

    Adding doc comments

--- src/compile-template/index.js
+++ src/compile-template/index.js
@@ -1,8 +1,48 @@
 'use strict';
 
+/**
+ * @ngdoc module
+ * @name compile-template
+ * @module compile-template
+ */
 let moduleName = 'buildium.angular-ui.compiletemplate';
 
 angular.module(moduleName, [])
+/**
+ * @ngdoc directive
+ * @name bdCompileTemplate
+ * @module  compile-template
+ *
+ * @restrict A
+ *
+ * @description
+ * Compiles the HTML that is bound using ng-bing-html.
+ *
+ *
+ * @example
+     <example module="compileTemplateExample" name="bd-compile-template">
+         <file name="index.html">
+            <div ng-controller="ExampleController">
+                <label>Title
+                    <input type="text" ng-model="title" />
+                </label>
+                <label>Compile Template
+                 <textarea ng-model="dynamicHtml" style="display: block"></textarea>
+                </label>
+                <h2>Result</h2>
+                <div ng-bind-html="dynamicHtml" bd-compile-template></div>
+            </div>
+         </file>
+         <file name="script.js">
+            angular.module('compileTemplateExample', [])
+            .controller('ExampleController', ['$scope', function($scope) {
+                            $scope.title = 'Hello world';
+                            $scope.dynamicHtml = '<b>{{title}}</b>'
+                        }]);
+         </file>
+     </example>
+ *
+ */
 .directive('bdCompileTemplate', ['$compile', '$parse', function BdCompileTemplate($compile, $parse) {
     let directive = {};
 
