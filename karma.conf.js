9e419573969de08229f001e91615d51559932727
Edwin Chow <edwin.chow@buildium.com>
2017-04-04 15:05:28 -0400

    various config settings

--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
+    frameworks: ['jasmine', 'browserify'],
 
     // list of files / patterns to load in the browser
+
     files: [
-        '**/*.test.js'
+        'src/**/*.test.js'
     ],
 
 
@@ -27,6 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'src/**/*.js': ['browserify']
     },
 
 
@@ -55,8 +56,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
-
+    browsers: ['Electron'],
+    
+    electronOpts: {
+        show: false
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -64,6 +68,6 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: 1
   })
 }
