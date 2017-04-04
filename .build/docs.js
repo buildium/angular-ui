b756c8aaf672e21b83123fff346d1a4e4505611d
Calvin McEachron <calvin.mceachron@buildium.com>
2017-04-04 13:36:09 -0400

    [wip] live code in docs

--- .build/docs.js
+++ .build/docs.js
@@ -3,20 +3,70 @@
 var path = require('path');
 var docgen = require('dgeni-alive/src/docgen')();
 
-var config = function(log) {
-    log.level = 'info';
-};
+var packages = [
+  require('dgeni-alive/src/packages/jsdoc-ext'),
+  require('dgeni-alive/src/packages/ngdoc-ext'),
+  require('dgeni-alive/src/packages/links-ext'),
+  require('dgeni-alive/src/packages/examples-ext'),
+  require('dgeni-packages/git')
+];
 
-docgen.Package().config(config);
+var configPath = path.join(__dirname, 'docs');
+
+var docs = docgen.Package(packages)
+
+.config(function(log) {
+    log.level = 'info';
+})
+
+.config(function (generateExamplesProcessor, generateProtractorTestsProcessor) {
+    var deployments = {
+        name: 'default',
+        examples: {
+            commonFiles: {
+                scripts: [
+                    '//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js',
+                    'index.js'
+                  // 'dist/js/vendor.bundle.js',
+                  // 'dist/js/app.bundle.js'
+                ]
+            }
+        },
+        scripts: [
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js',
+            'index.js'
+          // '../dist/js/vendor.bundle.js',
+          // '../dist/js/app.bundle.js'
+        ],
+        stylesheets: [
+        ]
+    };
+    generateExamplesProcessor.deployments = [ deployments ];
+    generateProtractorTestsProcessor.deployments = [ deployments ];
+})
+
+.config(function (renderDocsProcessor) {
+    renderDocsProcessor.extraData.deploymentTarget = 'default';
+})
+
+.config(function(generateWebsiteProcessor) {
+  generateWebsiteProcessor.locals('productTitle', 'Buildium : Angular-UI');
+})
+
+.config(function(templateFinder, renderDocsProcessor, gitData) {
+  templateFinder.templateFolders.unshift(path.resolve(configPath, 'templates/examples'));
+  templateFinder.templateFolders.unshift(path.resolve(configPath, 'templates/ngdoc'));
+  templateFinder.templateFolders.unshift(path.resolve(configPath, 'templates/app'));
+  renderDocsProcessor.extraData.git = gitData;
+});
 
 var sourceFiles = [
+    path.join(__dirname, '../src/**/*.js'),
     path.join(__dirname, '../index.js'),
-    path.join(__dirname, '../src/**/*.js')
 ];
 
 var destination = path.join(__dirname, '../docs');
 
-docgen
-    .src(sourceFiles)
+docgen.src(sourceFiles)
     .dest(destination)
     .generate();
--- src/compile-dynamic-html/index.js
+++ src/compile-dynamic-html/index.js
@@ -10,14 +10,40 @@ angular.module(moduleName, [])
 /**
  * @ngdoc directive
  * @name bdCompileDynamicHtml
+ * @module  compile-dynamic-html
+ *
  * @restrict A
+ *
  * @description
- *     Replaces the contents of the element with the result of compiling the html string.
- *     Updates on changes to any contained scope variables
+ * Replaces the contents of the element with the result of compiling the html string.
+ * Updates on changes to any contained scope variables.
+ *
+ * @param {string} bdCompileDynamicHtml AngularJS expression evaluating to the html
+ *                                      that should be rendered in this container.
+ *
  * @example
- *     ```html
- *     <div bd-compile-dynamic-html="'<h1>{{title}}</h1>'"></div>
- *     ```
+    <example module="compileDynamicHtmlExample" name="bd-compile-dynamic-html">
+        <file name="index.html">
+            <div ng-controller="ExampleController">
+                <label>Title
+                    <input type="text" ng-model="title" />
+                </label>
+                <label>Dynamic Template
+                    <textarea ng-model="dynamicHtml" style="display: block"></textarea>
+                </label>
+                <h2>Result</h2>
+                <div bd-compile-dynamic-html="dynamicHtml"></div>
+            </div>
+        </file>
+        <file name="script.js">
+            angular.module('compileDynamicHtmlExample', [])
+                .controller('ExampleController', ['$scope', function($scope) {
+                    $scope.title = 'Hello world';
+                    $scope.dynamicHtml = '<b>{{title}}</b>'
+                }]);
+        </file>
+    </example>
+ *
  */
 .directive('bdCompileDynamicHtml', function CompileDynamicHtml() {
     let directive = {};
