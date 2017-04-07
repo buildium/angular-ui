3d6fda911e7f982d87b7d5562304923a4bf81688
Ryan Pierce <ryan.pierce@buildium.com>
2017-04-07 14:37:04 -0400

    Introducing babel

---
+++ src/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let moduleName = 'buildium.angular-ui';
+
+/**
+ * @ndgoc module
+ * @name angular-ui
+ * @module angular-ui
+ */
+angular.module(moduleName, [
+    require('./compile-dynamic-html'),
+    require('./scroll-into-view'),
+    require('./event'),
+    require('./copy-to-clipboard'),
+    require('./loading-src'),
+    require('./compile-template')
+]);
+
+module.exports = moduleName;
